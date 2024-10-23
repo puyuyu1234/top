188ca086ea8d652f906cdbbcac54d7e4bb67dc3b
puyuyu1234 <145762224+puyuyu1234@users.noreply.github.com>
2024-10-23 13:36:20 +0900

    fixOpinionBox

--- opinionBox/main.js
+++ opinionBox/main.js
@@ -1,45 +1,57 @@
 "use strict";
 
-const URL =
-    "https://script.google.com/macros/s/AKfycbyGeF2DocntYyKk0RW6awY-T8skNEuYlYags9o8yCrnvVBDWR6MxFT0aO3QmUtq129g/exec";
-// データを書き込むための関数
-async function writeData(value) {
-    const response = await fetch(URL, {
+const endPoint =
+    "https://script.google.com/macros/s/AKfycbynZqb6uY0t72beiLxz8tJ-ptowVbGC1_WtZABJZ9NIojPkzMTX7MMpRifMLUX8v0Y_ew/exec";
+
+const writeData = async (value) => {
+    const data = {
         method: "POST",
-        headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        header: {
+            "Content-Type": "application/x-www-form-urlencoded",
         },
-        body: JSON.stringify({
-            action: "write",
-            value: value,
-        }),
-    });
-
+        body: JSON.stringify(value),
+    };
+    const response = await fetch(endPoint, data);
     const result = await response.json();
-    console.log(result);
-}
+    if (result.status == "success") {
+        alert("正常に送信されました");
+    } else {
+        alert("正常に送信できませんでした");
+    }
+};
 
-// データを取得するための関数
-async function readData() {
-    const response = await fetch(URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            action: "read",
-        }),
-    });
-
+const readData = async () => {
+    const data = {
+        method: "GET",
+    };
+    const response = await fetch(endPoint, data);
     const result = await response.json();
-    console.log(result);
-}
+
+    result.data.forEach((ntd, id) => {
+        const [name, text, dateText] = ntd;
+        const date = new Date(dateText);
+
+        // テキスト追加
+        {
+            const div = document.createElement("div");
+            div.className = "opinion";
+            const n = `<p class="name">${1 + id} : ${name}</p>`;
+            const d = `<p class="date">${date.toLocaleString()}</p>`;
+            const t = `<p class="text">${text}</p>`;
+            div.innerHTML = `${n}${t}${d}`;
+            board.appendChild(div);
+        }
+    });
+};
 
 sendButton.addEventListener("click", () => {
+    const n = nameInput.value;
     const t = textInput.value;
-    if (t != "") {
-        writeData([nameInput.value, t]);
+    if (t) {
+        writeData([n, t]);
+    } else {
+        alert("本文が空です！");
     }
 });
 
-readData()
+readData();
