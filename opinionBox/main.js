78db45d4aba9848742c974bd7688c0b88830776d
puyuyu1234 <145762224+puyuyu1234@users.noreply.github.com>
2025-02-27 19:39:51 +0900

    Update main.js

--- opinionBox/main.js
+++ opinionBox/main.js
@@ -1,78 +1,75 @@
 "use strict";
 
 const endPoint =
-    "https://script.google.com/macros/s/AKfycbynZqb6uY0t72beiLxz8tJ-ptowVbGC1_WtZABJZ9NIojPkzMTX7MMpRifMLUX8v0Y_ew/exec";
+    "https://api.github.com/repos/puyuyu1234/top/actions/workflows/update-messages.yml/dispatches";
 
 const sendButton = document.getElementById("sendButton");
 
 sendButton.addEventListener("click", () => {
-    const n = nameInput.value;
-    const t = textInput.value;
-    if (t) {
-        sendButton.disabled = true;
-        sendButton.innerText = "送信中……";
-        writeData([n, t]);
-    } else {
+    const name = nameInput.value;
+    const message = textInput.value;
+    if (!message) {
         alert("本文が空です！");
+        return;
     }
-});
-
-const writeData = async (value) => {
-    const data = {
+    sendButton.disabled = true;
+    sendButton.innerText = "送信中……";
+    fetch(endPoint, {
         method: "POST",
-        header: {
-            "Content-Type": "application/x-www-form-urlencoded",
+        headers: {
+            Accept: "application/vnd.github.v3+json",
+            Authorization: "Bearer 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",
         },
-        body: JSON.stringify(value),
-    };
-    fetch(endPoint, data)
+        body: JSON.stringify({
+            ref: "main",
+            inputs: { name, message },
+        }),
+    })
         .then((response) => {
             if (response.ok) {
                 alert("送信しました。");
             } else {
-                alert(`エラーコード：${response.status}`);
+                alert(`送信エラー\nエラーコード：${response.status}`);
             }
         })
         .catch((error) => {
-            alert(error);
+            alert(`送信エラー\n${error}`);
         })
         .finally(() => {
             sendButton.disabled = false;
             sendButton.innerText = "送信";
         });
-};
+});
 
 const board = document.getElementById("board");
-const readData = async () => {
-    const data = {
-        method: "GET",
-    };
-    const result = await fetch(endPoint, data).then((response) => response.json());
+const readData = () => {
+    const url = "messages.json";
+    fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+            return json.map((obj, index) => {
+                const name = obj.name;
+                const text = obj.text;
+                const date = new Date(obj.timestamp);
 
-    const opinionDivList = result.data
-        .map(([name, text, dateText], index) => {
-            const date = new Date(dateText);
-            const div = document.createElement("div");
-            div.className = "opinion";
-            const n = document.createElement("p");
-            n.className = "name";
-            n.innerText = `${1 + index} : ${name}`;
-            const d = document.createElement("p");
-            d.className = "date";
-            d.innerText = date.toLocaleString();
-            const t = document.createElement("p");
-            t.className = "text";
-            t.innerText = text;
-            div.append(n, t, d);
+                const div = document.createElement("div");
+                div.className = "opinion";
+                const n = document.createElement("p");
+                n.className = "name";
+                n.innerText = `${1 + index} : ${name}`;
+                const d = document.createElement("p");
+                d.className = "date";
+                d.innerText = date.toLocaleString();
+                const t = document.createElement("p");
+                t.className = "text";
+                t.innerText = text;
+                div.append(n, t, d);
 
-            return div;
+                return div;
+            });
         })
-        .toReversed();
-
-    // スケルトン削除
-    {
-        board.replaceChildren(...opinionDivList);
-    }
+        .then((divs) => {
+            board.replaceChildren(...divs);
+        });
 };
-
 readData();
