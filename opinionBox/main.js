09a4e4d1efa2a5835e43cc139f34c0c28420a9e1
puyuyu1234 <145762224+puyuyu1234@users.noreply.github.com>
2025-02-27 19:53:31 +0900

    Update main.js

--- opinionBox/main.js
+++ opinionBox/main.js
@@ -1,75 +1,75 @@
 "use strict";
 
 const endPoint =
-    "https://api.github.com/repos/puyuyu1234/top/actions/workflows/update-messages.yml/dispatches";
+  "https://api.github.com/repos/puyuyu1234/top/actions/workflows/update-messages.yml/dispatches";
 
 const sendButton = document.getElementById("sendButton");
 
 sendButton.addEventListener("click", () => {
-    const name = nameInput.value;
-    const message = textInput.value;
-    if (!message) {
-        alert("本文が空です！");
-        return;
-    }
-    sendButton.disabled = true;
-    sendButton.innerText = "送信中……";
-    fetch(endPoint, {
-        method: "POST",
-        headers: {
-            Accept: "application/vnd.github.v3+json",
-            Authorization: "Bearer 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",
-        },
-        body: JSON.stringify({
-            ref: "main",
-            inputs: { name, message },
-        }),
+  const name = nameInput.value;
+  const message = textInput.value;
+  if (!message) {
+    alert("本文が空です！");
+    return;
+  }
+  sendButton.disabled = true;
+  sendButton.innerText = "送信中……";
+  fetch(endPoint, {
+    method: "POST",
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+      Authorization: "Bearer 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",
+    },
+    body: JSON.stringify({
+      ref: "main",
+      inputs: { name, message },
+    }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("送信しました。");
+      } else {
+        alert(`送信エラー\nエラーコード：${response.status}`);
+      }
     })
-        .then((response) => {
-            if (response.ok) {
-                alert("送信しました。");
-            } else {
-                alert(`送信エラー\nエラーコード：${response.status}`);
-            }
-        })
-        .catch((error) => {
-            alert(`送信エラー\n${error}`);
-        })
-        .finally(() => {
-            sendButton.disabled = false;
-            sendButton.innerText = "送信";
-        });
+    .catch((error) => {
+      alert(`送信エラー\n${error}`);
+    })
+    .finally(() => {
+      sendButton.disabled = false;
+      sendButton.innerText = "送信";
+    });
 });
 
 const board = document.getElementById("board");
 const readData = () => {
-    const url = "messages.json";
-    fetch(url)
-        .then((response) => response.json())
-        .then((json) => {
-            return json.map((obj, index) => {
-                const name = obj.name;
-                const text = obj.text;
-                const date = new Date(obj.timestamp);
+  const url = "messages.json";
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      return json.map((obj, index) => {
+        const name = obj.name;
+        const text = obj.text;
+        const date = new Date(obj.timestamp);
 
-                const div = document.createElement("div");
-                div.className = "opinion";
-                const n = document.createElement("p");
-                n.className = "name";
-                n.innerText = `${1 + index} : ${name}`;
-                const d = document.createElement("p");
-                d.className = "date";
-                d.innerText = date.toLocaleString();
-                const t = document.createElement("p");
-                t.className = "text";
-                t.innerText = text;
-                div.append(n, t, d);
+        const div = document.createElement("div");
+        div.className = "opinion";
+        const n = document.createElement("p");
+        n.className = "name";
+        n.innerText = `${1 + index} : ${name}`;
+        const d = document.createElement("p");
+        d.className = "date";
+        d.innerText = date.toLocaleString();
+        const t = document.createElement("p");
+        t.className = "text";
+        t.innerText = text;
+        div.append(n, t, d);
 
-                return div;
-            });
-        })
-        .then((divs) => {
-            board.replaceChildren(...divs);
-        });
+        return div;
+      });
+    })
+    .then((divs) => {
+      board.replaceChildren(...divs);
+    });
 };
 readData();
